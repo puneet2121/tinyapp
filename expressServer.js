acbfb8d52c565d11d5810f4f4feaf4a8d0078ce3
puneet2121 <bedipuneet21@gmail.com>
2022-05-17 15:04:24 +0000

    adding parse and genrating random string

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({entended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,6 +19,11 @@ app.get('/urls',(req,res) => {
 
 //get http://localhost:8080/urls/b2xVn2
 //get http://localhost:8080/urls/9sm5xK
+
+app.get('/urls/new',(req,res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:myshortURL',(req,res) => {
   const templeVars = {
     shortURL: req.params.myshortURL,
@@ -23,12 +32,27 @@ app.get('/urls/:myshortURL',(req,res) => {
   res.render('urls_show',templeVars);
 })
 
+
 app.get('/hello',(req,res) => {
   const templateVars = {greeting: 'Hello World'};
   res.render('hello_world',templateVars)
 });
-
-
+app.post('/urls',(req,res) => {
+  console.log(req.body);
+  res.send('ok');
+});
+function generateRandomString() {
+  let ranString = ''
+  let char = 'ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst$%&*0123456789'
+  let charLength = char.length
+  for(let i = 0; i <= 6; i++){
+    ranString += characters.charAt(Math.floor(Math.random() * 
+ charLength));
+   }
+   return ranString;
+}
+generateRandomString();
+ 
 app.listen(port,() =>{
   console.log(`server is listening to port ${port}`)
 });
