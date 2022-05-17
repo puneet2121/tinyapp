1352c6176874da259faa9d93f4938cd165995f95
puneet2121 <bedipuneet21@gmail.com>
2022-05-17 00:32:31 +0000

    printing hello world using ejs template

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
 app.set('view engine','ejs');
+app.get('/urls',(req,res) => {
+  const templeVars = {urls: urlDatabase};
+  res.render('urls_index',templeVars);
+});
+app.get('/hello',(req,res) => {
+  const templateVars = {greeting: 'Hello World'};
+  res.render('hello_world',templateVars)
+});
+
+
+app.listen(port,() =>{
+  console.log(`server is listening to port ${port}`)
+});
