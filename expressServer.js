52ad9ff1b72f394b8bc473e4945e0d6a22c87af7
puneet2121 <bedipuneet21@gmail.com>
2022-05-17 00:01:24 +0000

    setting ejs as view engine

--- expressServer.js
+++ expressServer.js
@@ -2,17 +2,4 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const urlDatabse = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
-
-};
-app.get('/hello',(req,res) => {
-  res.send('<html><body>Hello<b>World</b></body></html>\n');
-});
-app.get('/urls.json',(req,res) => {
-  res.json(urlDatabse);
-});
-app.listen(port,() => {
-  console.log(`app listening on port ${port}`);
-});
+app.set('view engine','ejs');
