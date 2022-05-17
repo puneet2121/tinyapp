a5fdc6e5c65de6abc9ed9d6e90d4c32335c5d020
puneet2121 <bedipuneet21@gmail.com>
2022-05-17 02:34:52 +0000

    adding second url to the expressServer file

--- expressServer.js
+++ expressServer.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 app.set('view engine','ejs');
 app.get('/urls',(req,res) => {
   const templeVars = {urls: urlDatabase};
   res.render('urls_index',templeVars);
 });
+
+//get http://localhost:8080/urls/b2xVn2
+//get http://localhost:8080/urls/9sm5xK
+app.get('/urls/:myshortURL',(req,res) => {
+  const templeVars = {
+    shortURL: req.params.myshortURL,
+    longURL:urlDatabase[req.params.myshortURL]
+  };
+  res.render('urls_show',templeVars);
+})
+
 app.get('/hello',(req,res) => {
   const templateVars = {greeting: 'Hello World'};
   res.render('hello_world',templateVars)
