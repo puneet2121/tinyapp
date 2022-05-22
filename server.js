caf0a47c628dca45883d910fe99ac588904962c2
puneet2121 <bedipuneet21@gmail.com>
2022-05-22 23:08:55 +0000

    adding .DS_store in gitignore file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 app.set('view engine','ejs');
-app.get('/',function(req,res){
+app.get('/',function(req,res) {
   res.render('pages/index');
 });
-app.get('/about',function(req,res){
-  res.render('pages/about'); 
+app.get('/about',function(req,res) {
+  res.render('pages/about');
 });
 app.listen(8080);
 console.log('server is listening to 8080');
